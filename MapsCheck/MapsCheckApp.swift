4f32c90ce38c24ee51b8dbb637194aca546c519c
Konstantin Bondar <bondar.k.dev@gmail.com>
2024-02-17 12:12:52 +0100

    Initial Commit

---
+++ MapsCheck/MapsCheckApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapsCheckApp.swift
+//  MapsCheck
+//
+//  Created by Konstantin Bondar on 17.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct MapsCheckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
